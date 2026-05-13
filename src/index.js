7d82fbfa5407279fd30d5b06aadb65e25dbc306e
Ahmad Dhani <achmaddhani091@gmail.com>
2026-05-13 19:07:28 +0700

    mengupdate pembelajaran webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import * as bootstrap from 'bootstrap';
+import css from './style.scss';
 import {run} from './app/app.js';
 import {AlertService} from './app/alert.service.js';
 import {CalculatorService} from './app/calculator.service.js';
@@ -8,3 +10,4 @@ const calculatorService = new CalculatorService();
 const jokesService = new JokesService();
 
 run(alertService, calculatorService, jokesService);
+
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "output"),
+    filename: "bundle.js",
+  },
+  watch: true,
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: [["@babel/preset-env"]],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
